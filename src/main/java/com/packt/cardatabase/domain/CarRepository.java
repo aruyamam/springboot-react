76bf2660f6a12b30484a50c827ac1d7660023ee3
aruyamam <aruyamam@gmail.com>
2019-05-27 17:32:41 +0900

    Creating CRUD repositories

---
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,10 @@
+package com.packt.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+    // Fetch cars by brand and sort by year
+    List<Car> findByBrandOrderByYearsAsc(String brand);
+}
