aa05878b7221f00fd7e8173b06288d9d4f745a03
aruyamam <aruyamam@gmail.com>
2019-05-29 16:38:15 +0900

    Creating a RESTful web service

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -10,11 +10,14 @@ public class Car {
     private String brand, model, color, registerNumber;
     private int year, price;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "owner")
     private Owner owner;
 
+    public Car() {}
+
     public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
+        super();
         this.brand = brand;
         this.model = model;
         this.color = color;
